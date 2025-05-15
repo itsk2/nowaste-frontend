f88b544469ce51ef7778a8ed164057d27d5b7c2b
JunnieYAHH <gerelitopuyos02@gmail..com>
2025-05-15 11:52:16 +0800

    hello

--- frontend/assets/common/baseURL.js
+++ frontend/assets/common/baseURL.js
@@ -4,10 +4,10 @@ let baseURL = '';
 {
     Platform.OS == 'android'
 
-    ? baseURL = 'http://192.168.100.108:8080/api/v1'
-    : baseURL = 'http://192.168.137.187:8080/api/v1'
+    // ? baseURL = 'http://192.168.100.108:8080/api/v1'
+    // : baseURL = 'http://192.168.137.187:8080/api/v1'
 
-    ? baseURL = 'http://192.168.22.154:8080/api/v1'
+    ? baseURL = 'https://nw-mobile-backend-1.onrender.com/api/v1'
     : baseURL = 'https://nw-mobile-backend-1.onrender.com/api/v1'
 }
 export default baseURL;
