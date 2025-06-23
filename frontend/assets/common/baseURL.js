337f34cf68b52ea0669f275d0987bf0d980e50a7
JunnieYAHH <gerelitopuyos02@gmail..com>
2025-06-23 08:20:10 +0800

    commit stash

--- frontend/assets/common/baseURL.js
+++ frontend/assets/common/baseURL.js
@@ -3,11 +3,10 @@ import { Platform } from 'react-native'
 let baseURL = '';
 {
     Platform.OS == 'android'
-
-        ? baseURL = 'http://192.168.100.108:8080/api/v1'
-        : baseURL = 'http://192.168.26.154:8080/api/v1'
-
         // ? baseURL = 'https://nw-mobile-backend-1.onrender.com/api/v1'
         // : baseURL = 'https://nw-mobile-backend-1.onrender.com/api/v1'
+        
+        ? baseURL = 'http://192.168.20.180:8080/api/v1'
+        : baseURL = 'http://192.168.20.180:8080/api/v1'
 }
 export default baseURL;
